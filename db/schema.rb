5b70ed133a34a4b5fc7d4511b9ba8ad69454b9ea
Kurilov Arkadiy <kac.kurilov@yandex.ru>
2018-06-08 10:00:57 +0300

    Answer model has been created

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,15 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2018_06_08_064630) do
+ActiveRecord::Schema.define(version: 2018_06_08_065835) do
+
+  create_table "answers", force: :cascade do |t|
+    t.string "text"
+    t.integer "user_id"
+    t.boolean "best"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
 
   create_table "categories", force: :cascade do |t|
     t.string "title"
