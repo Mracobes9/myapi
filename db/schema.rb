45b6fd9fc19d547d48a7ce47091b6e28f3158b04
Kurilov Arkadiy <kac.kurilov@yandex.ru>
2018-06-08 09:53:06 +0300

    Category seed task has been created

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,13 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2018_06_07_113245) do
+ActiveRecord::Schema.define(version: 2018_06_08_064630) do
+
+  create_table "categories", force: :cascade do |t|
+    t.string "title"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
 
   create_table "users", force: :cascade do |t|
     t.string "email", null: false
--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,8 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+require 'faker'
+
+20.times do |i|
+    Category.create(title: Faker::ProgrammingLanguage.name)
+end
